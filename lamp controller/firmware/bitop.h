a1f759cc91598a31994f073d38a8e0d6441823f9
Alexandru Stan <amstan@csclub.uwaterloo.ca>
2010-11-09 23:28:16 -0500

    tried to make a test program

---
+++ lamp controller/firmware/bitop.h
@@ -0,0 +1,23 @@
+#ifndef BITOP_H
+#define BITOP_H
+
+/*! @file bitop.h
+ * Bit Operations Library
+ */
+
+/// Clear bit
+#define clear_bit(reg, bitNumb)           ((reg) &= ~(1 << (bitNumb)))
+
+/// Set bit
+#define set_bit(reg, bitNumb)             ((reg) |= (1 << (bitNumb)))
+
+/// Toggle bit
+#define toggle_bit(reg, bitNumb)          ((reg) ^= (1 << (bitNumb)))
+
+/// Test bit
+#define test_bit(reg, bitNumb)            ((reg) & (1 << (bitNumb)))
+
+/// Change Bit
+#define change_bit(reg, bitNumb, val)     {if (val) set_bit(reg, bitNumb); else clear_bit(reg,bitNumb);}
+
+#endif
--- lamp controller/firmware/main.c
+++ lamp controller/firmware/main.c
@@ -1,5 +1,21 @@
-char test;
+#include <pic16f88.h>
+#include "bitop.h"
+
+void delay(int count) {
+	int i,j;
+	for(i=0;i<count;i++)
+		for(j=0;j<10000;j++);
+}
 
 void main(void) {
-	test=0;
+	//initialize the ports
+	TRISB=0b1111111;
+	TRISA=0b1111110;
+	PORTA=0b0000000;
+	PORTB=0b0000000;
+	
+	while(1) {
+		delay(10);
+		toggle_bit(PORTA,0);
+	}
 }
