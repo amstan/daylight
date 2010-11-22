fb9209e88b3af57fc25feecbbdeb2d998601c97f
Alexandru Stan <amstan@csclub.uwaterloo.ca>
2010-11-21 22:54:42 -0500

    added testing folder, started some testing for avr-i2c, moved miniterm in testing folder

---
+++ testing/avr-i2c/main.c
@@ -0,0 +1,78 @@
+#include <avr/io.h>
+#include <avr/pgmspace.h>
+#include <util/delay.h>
+
+#include "bitop.h"
+
+#include "stdlib.h"
+#include "stdio.h"
+#include "uart.h"
+
+#include "i2cmaster.h"
+
+#define COLOURPORT PORTA
+#define RED 0
+#define GREEN 1
+#define BLUE 2
+
+#define I2C_ADD 0b11010000
+unsigned char i2c_get(unsigned char reg) {
+	char ret;
+	ret = i2c_start(I2C_ADD+I2C_WRITE);
+	if ( ret ) {
+		i2c_stop();
+		COLOURPORT=1<<RED;
+		printf("i2c_start error, %d\n",ret);
+	} else {
+		i2c_write(reg); //location
+		i2c_rep_start(I2C_ADD+I2C_READ);
+		ret = i2c_readNak(); //read the data
+		i2c_stop();
+	}
+	return ret;
+}
+
+int i2c_put(unsigned char reg,unsigned char data) {
+	char ret;
+	ret = i2c_start(I2C_ADD+I2C_WRITE);
+	if ( ret ) {
+		i2c_stop();
+		COLOURPORT=1<<RED;
+		printf("i2c_start error, %d\n",ret);
+	} else{
+		i2c_write(reg);  //location
+		i2c_write(data); //data
+		i2c_stop();
+	}
+	return ret;
+}
+
+
+FILE uart_str = FDEV_SETUP_STREAM(uart_putchar, uart_getchar, _FDEV_SETUP_RW); // This line is used to set up file access
+void setup(void) {
+	//Ports
+	DDRA=0b00000111;
+	PORTA=0b00000000;
+	
+	DDRB=0b00000000;
+	PORTB=0b00000000;
+	
+	//i2c
+	DDRC =0b00000011;
+	PORTC=0b00000011;
+	
+	//UART
+	uart_init();
+	stdout = stdin = &uart_str;
+	printf("Booted!\n");
+}
+
+
+int main(void) {
+	setup();
+	i2c_init();
+	
+	for(unsigned char i=0x00;i<=0x07;i++) {
+		printf("address %d, data %d\n\n",i,i2c_get(i));
+	}
+}
---
+++ testing/avr-i2c/uart.h
@@ -0,0 +1,55 @@
+#ifndef UART_H
+#define UART_H
+
+/*! @file uart.h
+ * UART Communication Library
+ *
+ * @author Joerg Wunsch
+ *
+ * @par License:
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <joerg@FreeBSD.ORG> wrote this file.  As long as you retain this notice you can do whatever you want with this stuff. If we meet some day, and you think this stuff is worth it, you can buy me a beer in return.
+ *
+ */
+
+/*! Perform UART startup initialization.
+ * Initialize the UART to 9600 Bd, tx/rx, 8N1.
+ */
+void uart_init(void);
+
+/*! Send character c down the UART Tx
+ * Wait until tx holding register is empty.
+ * @param c character to send
+ * @param stream file stream to putchar on
+ * @returns 
+ */
+int uart_putchar(char c, FILE *stream);
+
+/*
+ * Size of internal line buffer used by uart_getchar().
+ */
+#define RX_BUFSIZE 80
+
+/*!
+ * Receive a character from the UART Rx.
+ *
+ * This features a simple line-editor that allows to delete and re-edit the characters entered, until either CR or NL is entered. Printable characters entered will be echoed using uart_putchar().
+ *
+ * @par Editing characters:
+ * - \\b (BS) or \177 (DEL) delete the previous character
+ * - ^u kills the entire input buffer
+ * - ^w deletes the previous word
+ * - ^r sends a CR, and then reprints the buffer
+ * - \t will be replaced by a single space
+ *
+ * All other control characters will be ignored.
+ *
+ * The internal line buffer is RX_BUFSIZE (80) characters long, which includes the terminating \\n (but no terminating \0).  If the buffer is full (i. e., at RX_BUFSIZE-1 characters in order to keep space for the trailing \\n), any further input attempts will send a \\a to uart_putchar() (BEL character), although line editing is still allowed.
+ *
+ * Input errors while talking to the UART will cause an immediate return of -1 (error indication).  Notably, this will be caused by a framing error (e. g. serial line "break" condition), by an input overrun, and by a parity error (if parity was enabled and automatic parity recognition is supported by hardware).
+ *
+ * Successive calls to uart_getchar() will be satisfied from the internal buffer until that buffer is emptied again.
+ */
+int uart_getchar(FILE *stream);
+
+#endif
