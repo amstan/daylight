0c5bba25efba83b32aa545571f2b3554db88fbb8
Alexandru Stan <amstan@csclub.uwaterloo.ca>
2010-11-22 22:23:51 -0500

    added various libraries, including light sensor library

---
+++ contrib/light/light.h
@@ -0,0 +1,22 @@
+#ifndef LIGHT_H
+#define LGIHT_H   1
+
+#include "i2cmaster.h"
+
+//Library for the use with TCS3404 DIGITAL COLOR LIGHT SENSOR
+//Requires bitop.h and i2cmaster.h library
+
+#define I2C_ADD 0b01110010
+
+#define LIGHT_RED 0x12
+#define LIGHT_GREEN 0x10
+#define LIGHT_BLUE 0x14
+#define LIGHT_CLEAR 0x16
+
+int light_init(void);
+unsigned char light_read(unsigned char reg);
+int light_write(unsigned char reg,unsigned char data);
+unsigned int light_level(unsigned char reg);
+unsigned char light_ready(void);
+
+#endif
